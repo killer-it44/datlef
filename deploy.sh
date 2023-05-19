dd62a7632646f4ebd37dc0004c2f7c5689393e6f
Jens Keller <keller_jens@hotmail.com>
2023-05-19 19:34:14 +0800

    zero-downtime deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-cf push datlef --no-start
-cf map-route datlef internal.cfapps.sap.hana.ondemand.com --hostname cloud-native-dev-usage-tracker-analytics
-cf set-env datlef READONLY_DB_URL "$READONLY_DB_URL"
-cf bind-service datlef postgres-usage-tracker
-cf start datlef
+#!/bin/sh -e
+cf push datlef-new --no-start
+cf map-route datlef-new internal.cfapps.sap.hana.ondemand.com --hostname cloud-native-dev-usage-tracker-analytics
+cf set-env datlef-new READONLY_DB_URL "$READONLY_DB_URL"
+cf bind-service datlef-new postgres-usage-tracker
+cf start datlef-new
+cf delete -f datlef
+cf rename datlef-new datlef
