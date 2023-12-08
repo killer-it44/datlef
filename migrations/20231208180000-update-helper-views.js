99444d623c8b679c73a0196daba7c0cc00721cda
Jens Keller <keller_jens@hotmail.com>
2023-12-08 21:16:32 +0800

    make views more foolproof (user is a reserved keyword in pg)

---
+++ migrations/20231208180000-update-helper-views.js
@@ -0,0 +1,25 @@
+'use strict'
+
+exports.up = async function (db) {
+    await db.runSql('DROP VIEW views')
+    await db.runSql(`CREATE VIEW views AS 
+        SELECT group_name, site_name, user_id, instant as timestamp
+            FROM page INNER JOIN pagehit ON page.id = pagehit.page_id;
+    `)
+    await db.runSql('DROP VIEW clicks')
+    await db.runSql(`CREATE VIEW clicks AS 
+        SELECT user_id, instant AS timestamp, source_uri, target_uri FROM click;
+    `)
+}
+
+exports.down = async function (db) {
+    await db.runSql('DROP VIEW views')
+    await db.runSql(`CREATE VIEW views AS 
+    SELECT group_name, site_name, user_id as user, instant as timestamp
+        FROM page INNER JOIN pagehit ON page.id = pagehit.page_id;
+    `)
+    await db.runSql('DROP VIEW clicks')
+    await db.runSql(`CREATE VIEW clicks AS 
+        SELECT user_id AS user, instant AS timestamp, source_uri AS source_uri, target_uri as target_uri FROM click;
+    `)
+}
