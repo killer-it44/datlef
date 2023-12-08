2ebac970cfc141fa2b748c7d36411835ba0cd6fc
Jens Keller <keller_jens@hotmail.com>
2023-12-08 21:16:03 +0800

    make migration also work locally

--- migrations/20231208160000-revoke-select-on-tech-objects.js
+++ migrations/20231208160000-revoke-select-on-tech-objects.js
@@ -2,11 +2,18 @@
 
 const [user] = process.env.READONLY_DB_USER.split(':')
 
+async function revokeSelectIfExists(db, object) {
+    const result = await db.runSql(`SELECT * FROM pg_catalog.pg_tables WHERE tablename='${object}'`)
+    if (result.rows.length > 0) {
+        await db.runSql(`REVOKE SELECT ON ${object} FROM ${user}`)
+    }
+}
+
 exports.up = async function (db) {
-    await db.runSql(`REVOKE SELECT ON pg_stat_statements FROM ${user}`)
-    await db.runSql(`REVOKE SELECT ON databasechangeloglock FROM ${user}`)
-    await db.runSql(`REVOKE SELECT ON databasechangelog FROM ${user}`)
-    await db.runSql(`REVOKE SELECT ON migrations FROM ${user}`)
+    await revokeSelectIfExists(db, 'pg_stat_statements')
+    await revokeSelectIfExists(db, 'databasechangeloglock')
+    await revokeSelectIfExists(db, 'databasechangelog')
+    await revokeSelectIfExists(db, 'migrations')
 }
 
 exports.down = async function () {
